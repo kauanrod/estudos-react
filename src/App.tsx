a37b7a518445add5a6ee832fe86b16b7cf6b9fb2
Kauan Rodrigues <kauanrod@hotmail.com>
2026-09-24 22:18:33 -0300

    useState 2 (indentacao)

--- src/App.tsx
+++ src/App.tsx
@@ -22,26 +22,26 @@ export default function App() {
   }
 
   function adicionar() {
-    setContador(valorAtual => valorAtual+1)
+    setContador(valorAtual => valorAtual + 1)
   }
 
   function subtrair() {
     if (contador == 0) return;
-    setContador(valorAtual => valorAtual-1)
+    setContador(valorAtual => valorAtual - 1)
   }
   return (
     <div>
       <h1>Conhecendo useState</h1>
 
       <input placeholder="Nome" value={alunoInput} onChange={(e) => setAlunoInput(e.target.value)} />
-      <br/>
+      <br />
       <input placeholder="Idade" value={idadeInput} onChange={(e) => setIdadeInput(e.target.value)} />
       <br />
       <button onClick={mostrarAluno}>Mostrar aluno</button>
       <hr />
       {infoAluno?.nome && <h3>Bem vindo: {infoAluno?.nome}, idade: {infoAluno?.idade} anos</h3>}
       <h4>Contador com useState:</h4>
-      <br/>
+      <br />
       <button onClick={adicionar}>+</button>{contador}<button onClick={subtrair}>-</button>
     </div>
   )
