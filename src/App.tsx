9c2dfb61b95e3e7c22b782a3c4e3866497434346
Kauan Rodrigues <kauanrod@hotmail.com>
2026-09-24 22:18:04 -0300

    useState 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import { useState } from "react";
 
+interface InfoALunoProps {
+  nome?: string;
+  idade?: string;
+}
+
 export default function App() {
-  const [input, setInput] = useState("")
-  const [aluno, setAluno] = useState("")
-  const [idade, setIdade] = useState("")
+  const [alunoInput, setAlunoInput] = useState("")
+  const [idadeInput, setIdadeInput] = useState("")
+  const [contador, setContador] = useState(0)
+
+  const [infoAluno, setInfoAluno] = useState<InfoALunoProps>()
 
   function mostrarAluno() {
-    setAluno(input);
-    setIdade(idade);
+    setInfoAluno(
+      {
+        nome: alunoInput,
+        idade: idadeInput
+      }
+    )
+  }
+
+  function adicionar() {
+    setContador(valorAtual => valorAtual+1)
+  }
+
+  function subtrair() {
+    if (contador == 0) return;
+    setContador(valorAtual => valorAtual-1)
   }
   return (
     <div>
       <h1>Conhecendo useState</h1>
 
-      <input placeholder="Nome" value={input} onChange={(e) => setInput(e.target.value)} />
-      <input placeholder="Idade" value={idade} onChange={(e) => setInput(e.target.value)} />
+      <input placeholder="Nome" value={alunoInput} onChange={(e) => setAlunoInput(e.target.value)} />
+      <br/>
+      <input placeholder="Idade" value={idadeInput} onChange={(e) => setIdadeInput(e.target.value)} />
       <br />
       <button onClick={mostrarAluno}>Mostrar aluno</button>
       <hr />
-      {aluno && <h3>Bem vindo: {aluno}, idade: {idade}</h3>}
+      {infoAluno?.nome && <h3>Bem vindo: {infoAluno?.nome}, idade: {infoAluno?.idade} anos</h3>}
+      <h4>Contador com useState:</h4>
+      <br/>
+      <button onClick={adicionar}>+</button>{contador}<button onClick={subtrair}>-</button>
     </div>
   )
 }
